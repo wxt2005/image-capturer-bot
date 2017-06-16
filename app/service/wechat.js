2842232c3dc4ba5ac9782a44534d5698e59630f7
Botao <wxt2005@gmail.com>
2017-06-16 08:03:43 +0800

    temporarily use url to send pixiv image to wechat bot

--- app/service/wechat.js
+++ app/service/wechat.js
@@ -7,19 +7,13 @@ module.exports = app => {
       const { wechatEndpoint } = app.config;
 
       for (const resource of resources) {
-        // not supoort stream yet
-        if (resource.stream) {
-          continue;
-        }
-
-        console.log(resource);
-
         yield ctx.curl(wechatEndpoint, {
           method: 'POST',
           data: {
             fileUrl: resource.url,
             filename: resource.fileName,
           },
+          timeout: [ 10000, 30000 ],
         });
       }
     }
